5bdce8d4b9b041c92d8b0e7208c1949e9b32490e
Dave Russell <dave.kerr@gmail.com>
2022-11-27 03:50:57 +0000

    [T4] Display articles in UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  def index
+    @articles = ArticleCollection.new_from_endpoint.articles
+
+    render layout: false
+  end
 end
